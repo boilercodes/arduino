d87459b64033354804e37cbaac974077dcdd8e4f
rmenai <rami.menai@outlook.com>
2021-10-27 22:53:18 +0300

    Write base script

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,28 @@
 
 void setup()
 {
+    // Set the LED state.
+    pinMode(LED_BUILTIN, OUTPUT);
 }
+
+int i = 0;
+int max_blinks = 10;
 
 void loop()
 {
+    if (i < max_blinks)
+    {
+        // Turn on the LED.
+        digitalWrite(LED_BUILTIN, HIGH);
+        delay(500);
+
+        // Turn off the LED.
+        digitalWrite(LED_BUILTIN, LOW);
+        delay(500);
+        i++;
+    }
+    else if (i == max_blinks)
+    {
+        exit(0);
+    }
 }
